f4a167704ef5bdba3f073eda12e4311191b17b3c
Jesse Caunter <js.caunter@gmail.com>
2021-11-01 09:34:39 +1300

    Create public folder with index.html file

--- server/server.js
+++ server/server.js
@@ -1,5 +1,11 @@
+const path = require('path')
 const express = require('express')
 
 const server = express()
 
 module.exports = server
+
+server.get('*', (req, res) => {
+    const appPath = path.join(__dirname, 'public', 'index.html')
+    res.sendFile(appPath)
+})

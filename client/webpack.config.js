d82b81c3a24decfa838977c09fe3ba3dcc49d7a8
Jesse Caunter <js.caunter@gmail.com>
2021-11-02 10:53:32 +1300

    Fix configuration so that .jsx files can be resolved

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -13,12 +13,12 @@ module.exports = {
                 test: /\.jsx?$/,
                 exclude: /node_modules/,
                 use: {
-                    loader: 'babel-loader',
-                    options: {
-                        presets: ['@babel/preset-env']
-                    }
+                    loader: 'babel-loader'
                 }
             }
         ]
     },
+    resolve: {
+        extensions: ['.js', '.jsx']
+    }
 }

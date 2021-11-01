c673a0d1df51254582b4564693c71a34e800e7a9
Jesse Caunter <js.caunter@gmail.com>
2021-11-02 10:10:29 +1300

    Create basic index.js file in client directory

---
+++ client/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+document.addEventListener('DOMContentLoaded', () => {
+    ReactDOM.render(
+        <h1>Under Construction</h1>,
+        document.getElementById('app')
+    )
+})

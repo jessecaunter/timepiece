20320fa9ca5d656dfa8f4219d37016a4d457fc86
Jesse Caunter <js.caunter@gmail.com>
2021-11-02 10:41:30 +1300

    Add babel-loader to module list in webpack config file

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -5,5 +5,20 @@ module.exports = {
     output: {
         filename: 'bundle.js',
         path: path.join(__dirname, '../server/public')
-    }
+    },
+    mode: 'development',
+    module: {
+        rules: [
+            {
+                test: /\.jsx?$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env']
+                    }
+                }
+            }
+        ]
+    },
 }

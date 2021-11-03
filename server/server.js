9294270782294f1a33841abe58cf9b250b5bebc5
Jesse Caunter <js.caunter@gmail.com>
2021-11-04 10:23:22 +1300

    Add script in public HTML file which runs bundle.js

--- server/server.js
+++ server/server.js
@@ -5,6 +5,8 @@ const server = express()
 
 module.exports = server
 
+server.use(express.static(path.join(__dirname, 'public')))
+
 server.get('*', (req, res) => {
     const appPath = path.join(__dirname, 'public', 'index.html')
     res.sendFile(appPath)

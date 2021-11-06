845c32ca23c109e2da904e5721305f3a1090bb38
Jesse Caunter <js.caunter@gmail.com>
2021-11-06 16:50:56 +1300

    Add useEffect hook to update time every second

--- client/components/Clock.jsx
+++ client/components/Clock.jsx
@@ -1,8 +1,12 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 
 export default function Clock() {
 
     const [date, setDate] = useState(new Date())
+
+    useEffect(() => {
+        const timer = setInterval(() => setDate(new Date()), 1000)
+    })
 
     return (
         <h1>{date.toTimeString()}</h1>

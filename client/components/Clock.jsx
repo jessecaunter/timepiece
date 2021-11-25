b8f856351e37586e96a15ff1d39bf949915e3b84
Jesse Caunter <js.caunter@gmail.com>
2021-11-25 13:07:25 +1300

    Render lists for seconds, minutes and hours
    These will be used for the digital clock.

--- client/components/Clock.jsx
+++ client/components/Clock.jsx
@@ -1,9 +1,29 @@
 import React, { useState, useEffect } from 'react'
 
 export default function Clock() {
-
+    // Local state
     const [date, setDate] = useState(new Date())
+    // Other variables
+    const seconds = [...Array(24).keys()]
+    const minutes = [...Array(60).keys()]
+    const hours = minutes
+    const listSeconds = seconds.map(second =>
+        <li key={second.toString()}>
+            {second}
+        </li>
+    )
+    const listMinutes = minutes.map(minute =>
+        <li key={minute.toString()}>
+            {minute}
+        </li>
+    )
+    const listHours = hours.map(hour =>
+        <li key={hour.toString()}>
+            {hour}
+        </li>
+    )
 
+    // Effects
     useEffect(() => {
         const interval = setInterval(() => {
             setDate(new Date())
@@ -18,6 +38,17 @@ export default function Clock() {
                 <div className='second hand'></div>
                 <div className='minute hand'></div>
                 <div className='hour hand'></div>
+            </div>
+            <div className='digital-clock'>
+                <div className='second section'>
+                    <ul>{listSeconds}</ul>
+                </div>
+                <div className='minute section'>
+                    <ul>{listMinutes}</ul>
+                </div>
+                <div className='hour section'>
+                    <ul>{listHours}</ul>
+                </div>
             </div>
         </div>
     )
